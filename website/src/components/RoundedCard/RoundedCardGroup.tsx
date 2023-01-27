7acb1682b93db80b28f00b4ad4499e9d3a2fc4a3
Conner Gallagher <ConnerNGallagher@gmail.com>
2023-01-27 12:42:12 -0700

    docs: added props to RoundedCardGroup

--- website/src/components/RoundedCard/RoundedCardGroup.tsx
+++ website/src/components/RoundedCard/RoundedCardGroup.tsx
@@ -6,17 +6,35 @@ export interface RoundedCardGroupProps {
   items: RoundedCardProps[];
   cols?: number;
   sx?: any;
+  direction?: "row" | "row-reverse" | "column" | "column-reverse";
+  justifyContent?:
+    | "flex-start"
+    | "center"
+    | "flex-end"
+    | "space-between"
+    | "space-around"
+    | "space-evenly";
+  alignItems?: "flex-start" | "center" | "flex-end" | "stretch" | "baseline";
 }
 
 export default function RoundedCardGroup({
   items,
   cols,
   sx,
+  direction,
+  justifyContent,
+  alignItems,
 }: RoundedCardGroupProps) {
   // const lg = Math.floor(12 / Math.min(cols ?? 2, items.length));
 
   return (
-    <Grid container spacing={3}>
+    <Grid
+      container
+      spacing={3}
+      direction={direction ?? "row"}
+      justifyContent={justifyContent}
+      alignItems={alignItems}
+    >
       {items.map((item) => (
         <Grid
           item
