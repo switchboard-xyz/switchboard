2e69db007cd2a756b98683cbb253f713a959dc6b
Conner Gallagher <ConnerNGallagher@gmail.com>
2023-02-23 16:30:28 -0700

    published v2.1.2 of @switchboard-xyz/oracle

--- javascript/oracle/src/utils.ts
+++ javascript/oracle/src/utils.ts
@@ -1,5 +1,6 @@
 import os from "os";
 import path from "path";
+import xdg from "xdg-basedir";
 
 /** Sleep for a given number of milliseconds
  * @param ms number of milliseconds to sleep for
@@ -16,4 +17,22 @@ export function normalizeFsPath(fsPath: string) {
     : fsPath.startsWith("~")
     ? path.join(os.homedir(), fsPath.slice(1))
     : path.join(process.cwd(), fsPath);
+}
+
+export function getCacheDir() {
+  const appName = "@switchboard-xyz";
+  const home = os.homedir();
+
+  switch (os.platform()) {
+    case "win32":
+      return process.env.APPDATA
+        ? path.join(process.env.APPDATA, appName)
+        : null;
+    case "darwin":
+      return home ? path.join(home, "Library/Caches", appName) : null;
+    case "linux":
+      return xdg.cache ? path.join(xdg.cache, appName) : null;
+  }
+
+  return null;
 }
