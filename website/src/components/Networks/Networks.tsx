72be3e9a876b446edb3b4ed639044c3a35c243dd
Conner Gallagher <ConnerNGallagher@gmail.com>
2023-06-28 14:13:40 -0600

    wip

--- website/src/components/Networks/Networks.tsx
+++ website/src/components/Networks/Networks.tsx
@@ -7,107 +7,125 @@ import PublicKeyButton from "../PublicKeyButton";
 import { type IChainNetworkConfig, networks } from "@switchboard-xyz/common";
 
 import type { SupportedChain } from "./types";
+import { capitalizeFirstLetterOfEachWord, getNetworkTable } from "./utils";
 
 export interface IQueues {
   chain: SupportedChain;
 }
 
-export function getNetworkTable(
-  chain: SupportedChain,
-  network: string
-): [string, string, JSX.Element] {
-  const networkConfig: IChainNetworkConfig = networks[chain][network];
-  const networkName = networkConfig.networkName ?? capitalizeWords(network);
-  const addresses: Array<[string, string]> = [];
-  addresses.push(["Program ID", networkConfig.programId]);
-  if (networkConfig.authority) {
-    addresses.push(["Program Authority", networkConfig.authority]);
-  }
-  const ignoreKeys = [
-    "networkName",
-    "programId",
-    "authority",
-    "queues",
-    "metadata",
-  ];
-  const restOfKeys = Object.keys(networkConfig).filter(
-    (key) => !ignoreKeys.includes(key)
-  );
-  restOfKeys.forEach((key) =>
-    addresses.push([capitalizeWords(key), networkConfig[key] as string])
-  );
+// export function getNetworkTable(
+//   chain: SupportedChain,
+//   network: string
+// ): [string, string, JSX.Element] {
+//   const networkConfig: IChainNetworkConfig = networks[chain][network];
+//   const networkName = networkConfig.networkName ?? capitalizeWords(network);
+//   const addresses: Array<[string, string]> = [];
+//   addresses.push(["Program ID", networkConfig.programId]);
+//   if (networkConfig.authority) {
+//     addresses.push(["Program Authority", networkConfig.authority]);
+//   }
+//   const ignoreKeys = [
+//     "networkName",
+//     "programId",
+//     "authority",
+//     "queues",
+//     "metadata",
+//   ];
+//   const restOfKeys = Object.keys(networkConfig).filter(
+//     (key) => !ignoreKeys.includes(key)
+//   );
+//   restOfKeys.forEach((key) =>
+//     addresses.push([capitalizeWords(key), networkConfig[key] as string])
+//   );
 
-  const table = (
-    <>
-      <h3>Program Deployment</h3>
-      <p>
-        Below are the <i>{network}</i> {capitalizeWords(chain)} addresses
-        associated with the Switchboard deployment.
-      </p>
-      <table>
-        <tr>
-          <th>Account</th>
-          <th>Address</th>
-        </tr>
-        {addresses.map(([label, address]) => {
-          return (
-            <tr key={label}>
-              <td>{capitalizeWords(label)}</td>
-              <td>
-                <PublicKeyButton publicKey={address} />
-              </td>
-            </tr>
-          );
-        })}
-      </table>
+//   const table = (
+//     <>
+//       <h3>Program Deployment</h3>
+//       <p>
+//         Below are the <i>{network}</i> {capitalizeWords(chain)} addresses
+//         associated with the Switchboard deployment.
+//       </p>
+//       <table>
+//         <tr>
+//           <th>Account</th>
+//           <th>Address</th>
+//         </tr>
+//         {addresses.map(([label, address]) => {
+//           return (
+//             <tr key={label}>
+//               <td>{capitalizeWords(label)}</td>
+//               <td>
+//                 <PublicKeyButton publicKey={address} />
+//               </td>
+//             </tr>
+//           );
+//         })}
+//       </table>
 
-      <h3>Queues</h3>
+//       <h3>Queues</h3>
 
-      <table>
-        <tr>
-          <th>Queue</th>
-          <th>Address</th>
-        </tr>
+//       <table>
+//         <tr>
+//           <th>Queue</th>
+//           <th>Address</th>
+//         </tr>
 
-        {networkConfig.queues.map((queueConfig) => {
-          return (
-            <tr>
-              <td>{queueConfig.name}</td>
-              <td>
-                <PublicKeyButton publicKey={queueConfig.address} /> <br />
-                {queueConfig.permissioned ? (
-                  <p>
-                    The permissioned queue requires aggregators to have{" "}
-                    <code>PERMIT_ORACLE_QUEUE_USAGE</code> permissions before
-                    using the queue's resources.
-                  </p>
-                ) : (
-                  <p>
-                    The permissionless queue does not require aggregators to
-                    have <code>PERMIT_ORACLE_QUEUE_USAGE</code> permissions
-                    before using a queue's resources. This is the default queue
-                    when building feeds in the publisher.
-                  </p>
-                )}
-              </td>
-            </tr>
-          );
-        })}
-      </table>
-    </>
-  );
+//         {networkConfig.queues.map((queueConfig) => {
+//           return (
+//             <tr>
+//               <td>{queueConfig.name}</td>
+//               <td>
+//                 <PublicKeyButton publicKey={queueConfig.address} /> <br />
+//                 {queueConfig.permissioned ? (
+//                   <p>
+//                     The permissioned queue requires aggregators to have{" "}
+//                     <code>PERMIT_ORACLE_QUEUE_USAGE</code> permissions before
+//                     using the queue's resources.
+//                   </p>
+//                 ) : (
+//                   <p>
+//                     The permissionless queue does not require aggregators to
+//                     have <code>PERMIT_ORACLE_QUEUE_USAGE</code> permissions
+//                     before using a queue's resources. This is the default queue
+//                     when building feeds in the publisher.
+//                   </p>
+//                 )}
+//               </td>
+//             </tr>
+//           );
+//         })}
+//       </table>
+//     </>
+//   );
 
-  return [network, networkName, table];
+//   return [network, networkName, table];
+// }
+
+interface INetworkTable {
+  chain: string;
+  network: string;
+  networkName: string;
+  table: JSX.Element;
 }
 
 export default function Networks({ chain }: IQueues) {
-  const networkTables: [string, string, JSX.Element][] = Object.keys(
-    networks[chain]
-  ).map((network) => getNetworkTable(chain, network));
+  const ALL_NETWORKS = Object.keys(networks[chain]);
+  const data: INetworkTable[] = ALL_NETWORKS.map((n) => {
+    const config: IChainNetworkConfig = networks[chain][n];
+    const displayName =
+      config.networkName ?? capitalizeFirstLetterOfEachWord(n);
+    const table = getNetworkTable(chain, n);
+    return {
+      chain: chain,
+      network: n,
+      networkName: displayName,
+      table,
+    };
+  });
 
   return (
     <>
-      <Tabs defaultValue="mainnet" className="networks_tabs ">
+      <Tabs defaultValue="mainnet" className="networks_tabs">
         {networkTables.map(([label, displayName, table]) => {
           return (
             <TabItem value={label} label={displayName}>
--- website/src/components/Networks/NetworksGroup.tsx
+++ website/src/components/Networks/NetworksGroup.tsx
@@ -8,114 +8,10 @@ import {
   Grid,
 } from "@mui/material";
 import { SupportedChain } from "./types";
-import PublicKeyButton from "../PublicKeyButton";
-import NetworkTable from "./Networks";
+import { networks } from "@switchboard-xyz/common";
+import { getNetworkTable, capitalizeFirstLetterOfEachWord } from "./utils";
 
-// Assuming the data is imported from a JSON file.
-import { type IChainNetworkConfig, networks } from "@switchboard-xyz/common";
-
-function capitalizeFirstLetterOfEachWord(str: string): string {
-  const titleCase = str
-    .split(" ")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
-  if (titleCase.endsWith("dao")) {
-    return titleCase.slice(0, titleCase.length - 3) + "DAO";
-  }
-  return titleCase;
-}
-
-export function getNetworkTable(
-  chain: SupportedChain,
-  network: string
-): JSX.Element {
-  const networkConfig: IChainNetworkConfig = networks[chain][network];
-  const addresses: Array<[string, string]> = [];
-  addresses.push(["Program ID", networkConfig.programId]);
-  if (networkConfig.authority) {
-    addresses.push(["Program Authority", networkConfig.authority]);
-  }
-  const ignoreKeys = [
-    "networkName",
-    "programId",
-    "authority",
-    "queues",
-    "metadata",
-  ];
-  const restOfKeys = Object.keys(networkConfig).filter(
-    (key) => !ignoreKeys.includes(key)
-  );
-  restOfKeys.forEach((key) =>
-    addresses.push([
-      capitalizeFirstLetterOfEachWord(key),
-      networkConfig[key] as string,
-    ])
-  );
-
-  const table = (
-    <>
-      <h3>Program Deployment</h3>
-      <p>
-        Below are the <i>{network}</i> {capitalizeFirstLetterOfEachWord(chain)}{" "}
-        addresses associated with the Switchboard deployment.
-      </p>
-      <table>
-        <tr>
-          <th>Account</th>
-          <th>Address</th>
-        </tr>
-        {addresses.map(([label, address]) => {
-          return (
-            <tr key={label}>
-              <td>{capitalizeFirstLetterOfEachWord(label)}</td>
-              <td>
-                <PublicKeyButton publicKey={address} />
-              </td>
-            </tr>
-          );
-        })}
-      </table>
-
-      <h3>Queues</h3>
-
-      <table>
-        <tr>
-          <th>Queue</th>
-          <th>Address</th>
-        </tr>
-
-        {networkConfig.queues.map((queueConfig) => {
-          return (
-            <tr>
-              <td>{queueConfig.name}</td>
-              <td>
-                <PublicKeyButton publicKey={queueConfig.address} /> <br />
-                {queueConfig.permissioned ? (
-                  <p>
-                    The permissioned queue requires aggregators to have{" "}
-                    <code>PERMIT_ORACLE_QUEUE_USAGE</code> permissions before
-                    using the queue's resources.
-                  </p>
-                ) : (
-                  <p>
-                    The permissionless queue does not require aggregators to
-                    have <code>PERMIT_ORACLE_QUEUE_USAGE</code> permissions
-                    before using a queue's resources. This is the default queue
-                    when building feeds in the publisher.
-                  </p>
-                )}
-              </td>
-            </tr>
-          );
-        })}
-      </table>
-    </>
-  );
-
-  return table;
-}
-
-const SupportedNetworks = () => {
+const NetworksGroup = () => {
   const [selectedChain, setSelectedChain] = useState<SupportedChain>("solana");
   const [selectedNetwork, setSelectedNetwork] = useState<string>("mainnet");
   const [tableElement, setTableElement] = useState<JSX.Element>(
@@ -189,4 +85,4 @@ const SupportedNetworks = () => {
   );
 };
 
-export default SupportedNetworks;
+export default NetworksGroup;
