03f20f03ecb931d6b71d23ae601846a4b27cb8b7
Conner Gallagher <ConnerNGallagher@gmail.com>
2023-07-18 22:03:27 -0600

    docs

--- cli/src/aptos/BaseCommand.ts
+++ cli/src/aptos/BaseCommand.ts
@@ -24,7 +24,7 @@ import {
   OracleJob,
   SwitchboardDecimal,
 } from "@switchboard-xyz/common";
-import type { MaybeHexString } from "aptos";
+import type { AptosClient, MaybeHexString } from "aptos";
 import { AptosAccount, HexString } from "aptos";
 import fs from "fs";
 import YAML from "yaml";
@@ -88,7 +88,7 @@ export abstract class AptosBaseCommand
     });
   }
 
-  get aptos() {
+  get aptos(): AptosClient {
     return this.program.client;
   }
 
